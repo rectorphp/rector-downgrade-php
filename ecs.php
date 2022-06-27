72e2df9c4ca1a4b7dc8cbf2c2709dd96930a4a78
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-06-27 14:20:01 +0700

    copy ecs config

--- ecs.php
+++ ecs.php
@@ -2,13 +2,56 @@
 
 declare(strict_types=1);
 
+use PHP_CodeSniffer\Standards\Generic\Sniffs\CodeAnalysis\AssignmentInConditionSniff;
+use PhpCsFixer\Fixer\FunctionNotation\StaticLambdaFixer;
+use PhpCsFixer\Fixer\Phpdoc\GeneralPhpdocAnnotationRemoveFixer;
+use PhpCsFixer\Fixer\Phpdoc\NoSuperfluousPhpdocTagsFixer;
+use PhpCsFixer\Fixer\PhpUnit\PhpUnitStrictFixer;
+use Symplify\CodingStandard\Fixer\LineLength\DocBlockLineLengthFixer;
 use Symplify\EasyCodingStandard\Config\ECSConfig;
 use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 
 return static function (ECSConfig $ecsConfig): void {
     $ecsConfig->sets([SetList::PSR_12, SetList::SYMPLIFY, SetList::COMMON, SetList::CLEAN_CODE]);
 
-    $ecsConfig->paths([__DIR__ . '/src', __DIR__ . '/tests', __DIR__ . '/config', __DIR__ . '/ecs.php']);
+    $ecsConfig->paths([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+        __DIR__ . '/ecs.php',
+        __DIR__ . '/easy-ci.php',
+    ]);
 
-    $ecsConfig->skip(['*/Source/*', '*/Fixture/*', '*/Expected/*']);
+    $ecsConfig->ruleWithConfiguration(NoSuperfluousPhpdocTagsFixer::class, [
+        'allow_mixed' => true,
+    ]);
+
+    $ecsConfig->ruleWithConfiguration(GeneralPhpdocAnnotationRemoveFixer::class, [
+        'annotations' => [
+            'throw',
+            'throws',
+            'author',
+            'authors',
+            'package',
+            'group',
+            'required',
+            'phpstan-ignore-line',
+            'phpstan-ignore-next-line',
+        ],
+    ]);
+
+    $ecsConfig->rule(StaticLambdaFixer::class);
+
+    $ecsConfig->skip([
+        '*/Source/*',
+        '*/Fixture/*',
+        '*/Expected/*',
+
+        // buggy - @todo fix on Symplify master
+        DocBlockLineLengthFixer::class,
+
+        // breaking and handled better by Rector PHPUnit code quality set, removed in symplify dev-main
+        PhpUnitStrictFixer::class,
+
+        AssignmentInConditionSniff::class . '.FoundInWhileCondition',
+    ]);
 };
