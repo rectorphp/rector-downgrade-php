c857264716a90041d463f29970b8d43074c05dc8
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-06-29 20:59:59 +0700

    [DowngradePhp81] Add DowngradeEnumExistsRector (#12)
    * [DowngradePhp81] Add DowngradeEnumExistsRector
    
    * [ci-review] Rector Rectify
    
    * add @see for lookup
    
    * add @see for lookup
    
    Co-authored-by: GitHub Action <action@github.com>

---
+++ tests/DowngradePhp81/Rector/FuncCall/DowngradeEnumExistsRector/config/configured_rule.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\DowngradePhp81\Rector\FuncCall\DowngradeEnumExistsRector;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->import(__DIR__ . '/../../../../../../config/config.php');
+    $rectorConfig->rule(DowngradeEnumExistsRector::class);
+};
