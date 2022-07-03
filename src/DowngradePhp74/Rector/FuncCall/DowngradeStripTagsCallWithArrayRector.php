91011b4e8839eb53bf58afe2aae85d2cb9d34c3b
Tomas Votruba <tomas.vot@gmail.com>
2022-07-03 22:53:24 +0200

    fix FQN

--- src/DowngradePhp74/Rector/FuncCall/DowngradeStripTagsCallWithArrayRector.php
+++ src/DowngradePhp74/Rector/FuncCall/DowngradeStripTagsCallWithArrayRector.php
@@ -22,6 +22,7 @@ use PhpParser\Node\Scalar\String_;
 use Rector\Core\Rector\AbstractRector;
 use Rector\Naming\Naming\VariableNaming;
 use Rector\NodeTypeResolver\Node\AttributeKey;
+use Rector\PostRector\Collector\NodesToAddCollector;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
 
