f03147a5d31fdaff4416cef4af0661e42d4b0637
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-09-02 17:36:41 +0700

    [Testing] Update test to use yieldFilesFromDirectory

--- rules-tests/DowngradePhp72/Rector/ClassMethod/DowngradeParameterTypeWideningRector/EmptyConfigTest.php
+++ rules-tests/DowngradePhp72/Rector/ClassMethod/DowngradeParameterTypeWideningRector/EmptyConfigTest.php
@@ -19,7 +19,7 @@ final class EmptyConfigTest extends AbstractRectorTestCase
 
     public function provideData(): Iterator
     {
-        return $this->yieldFilePathsFromDirectory(__DIR__ . '/FixtureEmptyConfig');
+        return $this->yieldFilesFromDirectory(__DIR__ . '/FixtureEmptyConfig');
     }
 
     public function provideConfigFilePath(): string
