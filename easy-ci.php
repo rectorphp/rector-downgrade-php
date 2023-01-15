e0f853b5897ed20579f6d7d7c033c6e75d03d841
Tomas Votruba <tomas.vot@gmail.com>
2023-01-15 00:44:46 +0000

    cs

--- easy-ci.php
+++ easy-ci.php
@@ -6,7 +6,5 @@ use Rector\Core\Contract\Rector\RectorInterface;
 use Symplify\EasyCI\Config\EasyCIConfig;
 
 return static function (EasyCIConfig $easyCIConfig): void {
-    $easyCIConfig->typesToSkip([
-        RectorInterface::class,
-    ]);
+    $easyCIConfig->typesToSkip([RectorInterface::class]);
 };
