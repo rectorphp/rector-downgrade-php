3e9c70cfbf3ed9690a4d9612ad998ebd041d12c1
Tomas Votruba <tomas.vot@gmail.com>
2023-01-15 13:28:08 +0000

    cs

--- easy-ci.php
+++ easy-ci.php
@@ -6,10 +6,7 @@ use Rector\Core\Contract\Rector\RectorInterface;
 use Symplify\EasyCI\Config\EasyCIConfig;
 
 return static function (EasyCIConfig $easyCIConfig): void {
-    $easyCIConfig->paths([
-         __DIR__ . '/src',
-         __DIR__ . '/rules',
-    ]);
+    $easyCIConfig->paths([__DIR__ . '/src', __DIR__ . '/rules']);
 
     $easyCIConfig->typesToSkip([RectorInterface::class]);
 };
