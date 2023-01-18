e3e5b7fae0816b7abc58a4cddbb8b9404bf7d2de
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-01-18 18:33:22 +0700

    [DowngradePhp80] Add DowngradeInlinedAttributeRector

---
+++ rules-tests/DowngradePhp80/Rector/Param/DowngradeInlinedAttributeRector/config/configured_rule.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\DowngradePhp80\Rector\Param\DowngradeInlinedAttributeRector;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->import(__DIR__ . '/../../../../../../config/config.php');
+    $rectorConfig->rule(DowngradeInlinedAttributeRector::class);
+};
---
+++ rules/DowngradePhp80/Rector/Param/DowngradeInlinedAttributeRector.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\DowngradePhp80\Rector\Param;
+
+use PhpParser\Node;
+use PhpParser\Node\Param;
+use Rector\Core\Rector\AbstractRector;
+use Rector\NodeTypeResolver\Node\AttributeKey;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+/**
+ * @changelog https://php.watch/articles/php-attributes#syntax
+ */
+final class DowngradeInlinedAttributeRector extends AbstractRector
+{
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition('Refactor Inlined attribute markers to multiline so it marked as comment', [
+            new CodeSample(
+                <<<'CODE_SAMPLE'
+function grep(#[\JetBrains\PhpStorm\Language('RegExp')] string $pattern)
+{}
+CODE_SAMPLE
+                ,
+                <<<'CODE_SAMPLE'
+function grep(#[\JetBrains\PhpStorm\Language('RegExp')]
+string $pattern)
+CODE_SAMPLE
+            ),
+        ]);
+    }
+
+    /**
+     * @return array<class-string<Node>>
+     */
+    public function getNodeTypes(): array
+    {
+        return [Param::class];
+    }
+
+    /**
+     * @param Param  $node
+     */
+    public function refactor(Node $node): ?Node
+    {
+        $attributeGroups = $node->attrGroups;
+
+        if ($attributeGroups === []) {
+            return null;
+        }
+
+        $currentAttrGroups = current($attributeGroups);
+        $currentAttr = current($currentAttrGroups->attrs);
+
+        if ($currentAttr->getLine() === $node->getLine()) {
+            $node->setAttribute(AttributeKey::ORIGINAL_NODE, null);
+            return $node;
+        }
+
+        return $node;
+    }
+}
