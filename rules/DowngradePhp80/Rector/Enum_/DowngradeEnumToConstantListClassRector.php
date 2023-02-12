77d322b72fd76399edfa317cd4603e76690fa58c
Dmitrii Derepko <xepozz@list.ru>
2023-02-12 23:57:01 +0300

    Upgrade downgrading enum nodes

--- rules/DowngradePhp80/Rector/Enum_/DowngradeEnumToConstantListClassRector.php
+++ rules/DowngradePhp80/Rector/Enum_/DowngradeEnumToConstantListClassRector.php
@@ -5,17 +5,22 @@ declare(strict_types=1);
 namespace Rector\DowngradePhp80\Rector\Enum_;
 
 use PhpParser\Node;
+use PhpParser\Node\ComplexType;
+use PhpParser\Node\Expr\PropertyFetch;
 use PhpParser\Node\Identifier;
 use PhpParser\Node\Name;
 use PhpParser\Node\NullableType;
 use PhpParser\Node\Param;
-use PhpParser\Node\Stmt\Class_;
+use PhpParser\Node\Stmt\ClassLike;
 use PhpParser\Node\Stmt\ClassMethod;
 use PhpParser\Node\Stmt\Enum_;
+use PhpParser\Node\UnionType;
 use PHPStan\PhpDocParser\Ast\ConstExpr\ConstFetchNode;
 use PHPStan\PhpDocParser\Ast\PhpDoc\ParamTagValueNode;
 use PHPStan\PhpDocParser\Ast\Type\ConstTypeNode;
+use PHPStan\PhpDocParser\Ast\Type\IdentifierTypeNode;
 use PHPStan\PhpDocParser\Ast\Type\NullableTypeNode;
+use PHPStan\PhpDocParser\Ast\Type\UnionTypeNode;
 use PHPStan\Reflection\ClassReflection;
 use PHPStan\Reflection\ReflectionProvider;
 use Rector\BetterPhpDocParser\PhpDocInfo\PhpDocInfo;
@@ -39,28 +44,41 @@ final class DowngradeEnumToConstantListClassRector extends AbstractRector
 
     public function getRuleDefinition(): RuleDefinition
     {
-        return new RuleDefinition('Downgrade enum to constant list class', [
-            new CodeSample(
-                <<<'CODE_SAMPLE'
-enum Direction
+        return new RuleDefinition(
+            'Downgrades enum and all it\'s usages to constant list class replaced the enum references to the enum baking type',
+            [
+                new CodeSample(
+                    <<<'CODE_SAMPLE'
+enum Direction: string
 {
-    case LEFT;
+    case LEFT = 'left';
+    case RIGHT = 'right';
 
-    case RIGHT;
+    public function check(null|self|Enum|string $value): self
+    {
+        ...
+    }
 }
 CODE_SAMPLE
 
-                ,
-                <<<'CODE_SAMPLE'
+                    ,
+                    <<<'CODE_SAMPLE'
 class Direction
 {
     public const LEFT = 'left';
-
     public const RIGHT = 'right';
+    /**
+     * @param (null | \Direction::* | string) $value
+     */
+    public function check(null|string $value): string
+    {
+
+    }
 }
 CODE_SAMPLE
-            ),
-        ]);
+                ),
+            ]
+        );
     }
 
     /**
@@ -68,49 +86,41 @@ CODE_SAMPLE
      */
     public function getNodeTypes(): array
     {
-        return [Enum_::class, ClassMethod::class];
+        return [
+            Enum_::class,
+            ClassMethod::class,
+            PropertyFetch::class,
+        ];
     }
 
     /**
      * @param Enum_|ClassMethod $node
      */
-    public function refactor(Node $node): Class_|ClassMethod|null
+    public function refactor(Node $node): mixed
     {
         if ($node instanceof Enum_) {
             return $this->classFromEnumFactory->createFromEnum($node);
+        }
+        if ($node instanceof PropertyFetch) {
+            return $this->replaceEnumToConstant($node);
         }
 
         $hasChanged = false;
         $phpDocInfo = $this->phpDocInfoFactory->createFromNodeOrEmpty($node);
 
         foreach ($node->params as $param) {
-            if ($param->type instanceof Name) {
-                $paramType = $param->type;
-                $isNullable = false;
-            } elseif ($param->type instanceof NullableType) {
-                $paramType = $param->type->type;
-                $isNullable = true;
-            } else {
-                continue;
-            }
-
-            // is enum type?
-            /** @var string $typeName */
-            $typeName = $this->getName($paramType);
-            if (! $this->reflectionProvider->hasClass($typeName)) {
-                continue;
-            }
-
-            $classLikeReflection = $this->reflectionProvider->getClass($typeName);
-            if (! $classLikeReflection->isEnum()) {
-                continue;
-            }
-
-            $this->refactorParamType($classLikeReflection, $isNullable, $param);
+            $this->decorateParamDocType($param, $phpDocInfo);
+            $param->type = $this->renameEnumReference($param->type);
 
             $hasChanged = true;
+        }
+        if ($node->returnType !== null) {
+            $oldReturnType = $node->returnType;
 
-            $this->decorateParamDocType($classLikeReflection, $param, $phpDocInfo, $isNullable);
+            $node->returnType = $this->renameEnumReference($node->returnType);
+            if ($oldReturnType !== $node->returnType) {
+                $hasChanged = true;
+            }
         }
 
         if ($hasChanged) {
@@ -121,29 +131,100 @@ CODE_SAMPLE
     }
 
     private function decorateParamDocType(
-        ClassReflection $classReflection,
         Param $param,
         PhpDocInfo $phpDocInfo,
-        bool $isNullable
     ): void {
-        $constFetchNode = new ConstFetchNode('\\' . $classReflection->getName(), '*');
-        $constTypeNode = new ConstTypeNode($constFetchNode);
+        $nullable = false;
+        $paramType = $param->type;
+        if ($paramType instanceof UnionType) {
+            $types = [];
+            foreach ($paramType->types as $type) {
+                $classLikeReflection = $this->getClassReflectionByType($type);
+                if ($classLikeReflection === null) {
+                    $types[] = $type;
+                    continue;
+                }
+                $constFetchNode = new ConstFetchNode('\\' . $classLikeReflection->getName(), '*');
+                $types[$classLikeReflection->getName()] = new ConstTypeNode($constFetchNode);
+            }
+            $paramTypeNode = count($types) > 1 ? new UnionTypeNode($types) : new IdentifierTypeNode((string) $types[0]);
+        } else {
+            $nullable = $paramType instanceof NullableType;
+            $classLikeReflection = $this->getClassReflectionByType($paramType);
+            $typeName = $this->getName($paramType);
+            $typeNode = $classLikeReflection !== null
+                ? new ConstTypeNode(new ConstFetchNode('\\' . $classLikeReflection->getName(), '*'))
+                : new IdentifierTypeNode($typeName);
+            $paramTypeNode = $typeNode;
+        }
+        $paramTypeNode = $nullable ? new NullableTypeNode($paramTypeNode) : $paramTypeNode;
         $paramName = '$' . $this->getName($param);
-
-        $paramTypeNode = $isNullable ? new NullableTypeNode($constTypeNode) : $constTypeNode;
 
         $paramTagValueNode = new ParamTagValueNode($paramTypeNode, false, $paramName, '');
         $phpDocInfo->addTagValueNode($paramTagValueNode);
     }
 
-    private function refactorParamType(ClassReflection $classReflection, bool $isNullable, Param $param): void
+    private function replaceEnumToConstant(PropertyFetch $node): PropertyFetch|Node\Expr|Node\Scalar\String_
     {
-        $identifier = $this->enumAnalyzer->resolveType($classReflection);
-        if ($identifier instanceof Identifier) {
-            $param->type = $isNullable ? new NullableType($identifier) : new Name($identifier->name);
-        } else {
-            // remove type as ambiguous
-            $param->type = null;
+        if ((string) $node->name === 'value') {
+            return $node->var;
         }
+        if ((string) $node->name === 'name') {
+            return new Node\Scalar\String_($node->var->name->toString());
+        }
+
+        return $node;
+    }
+
+    private function renameEnumReference(null|ComplexType|Identifier|Name $type): null|ComplexType|Identifier|Name
+    {
+        if ($type instanceof UnionType) {
+            $types = [];
+            foreach ($type->types as &$innerType) {
+                $newType = $this->renameEnumReference($innerType);
+                $types[(string) $newType] = $newType;
+            }
+            $type->types = $types;
+            return $type;
+        }
+
+        $classLikeReflection = $this->getClassReflectionByType($type);
+        if ($classLikeReflection === null) {
+            return $type;
+        }
+        $identifier = $this->enumAnalyzer->resolveType($classLikeReflection);
+
+        if ($identifier !== null) {
+            if ($type instanceof NullableType) {
+                return new NullableType((string) $identifier);
+            }
+            return $identifier;
+        }
+
+        return null;
+    }
+
+    private function getClassReflectionByType(null|ComplexType|Identifier|Name $type,): ?ClassReflection
+    {
+        if ($type instanceof UnionType) {
+            foreach ($type->types as $type) {
+                $classReflection = $this->getClassReflectionByType($type);
+                if ($classReflection instanceof ClassReflection) {
+                    if ($classReflection->isEnum()) {
+                        return $classReflection;
+                    }
+                }
+            }
+            return null;
+        }
+        $typeName = $type instanceof NullableType ? (string) $type->type : (string) $type;
+        if (in_array($typeName, ['self', 'static'], true)) {
+            $class = $this->betterNodeFinder->findParentType($type, ClassLike::class);
+            return $this->reflectionProvider->getClass($this->getName($class));
+        }
+        if ($this->reflectionProvider->hasClass($typeName)) {
+            return $this->reflectionProvider->getClass($typeName);
+        }
+        return null;
     }
 }
