7eea3a570a97d6bb7bf6601b92833b69579d630e
GitHub Action <actions@github.com>
2023-05-26 22:36:14 +0000

    [ci-review] Rector Rectify

--- rules/DowngradePhp70/Rector/FuncCall/DowngradeDirnameLevelsRector.php
+++ rules/DowngradePhp70/Rector/FuncCall/DowngradeDirnameLevelsRector.php
@@ -123,9 +123,9 @@ CODE_SAMPLE
         $funcVariable = $this->namedVariableFactory->createVariable($funcCall, 'dirnameFunc');
 
         $closure = $this->createClosure();
-        $exprAssignClosure = $this->createExprAssign($funcVariable, $closure);
+        $expression = $this->createExprAssign($funcVariable, $closure);
 
-        $this->nodesToAddCollector->addNodeBeforeNode($exprAssignClosure, $funcCall);
+        $this->nodesToAddCollector->addNodeBeforeNode($expression, $funcCall);
 
         $funcCall->name = $funcVariable;
 
