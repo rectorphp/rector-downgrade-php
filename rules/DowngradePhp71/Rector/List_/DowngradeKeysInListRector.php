2d384c1dde85f4f8206539105520d92623ad3458
Tomas Votruba <tomas.vot@gmail.com>
2023-05-29 18:42:09 +0200

    Remove PARENT_NODE from DowngradeKeysInListRector

--- rules/DowngradePhp71/Rector/List_/DowngradeKeysInListRector.php
+++ rules/DowngradePhp71/Rector/List_/DowngradeKeysInListRector.php
@@ -12,13 +12,13 @@ use PhpParser\Node\Expr\ArrayItem;
 use PhpParser\Node\Expr\Assign;
 use PhpParser\Node\Expr\List_;
 use PhpParser\Node\Expr\Variable;
+use PhpParser\Node\Stmt;
 use PhpParser\Node\Stmt\Expression;
 use PhpParser\Node\Stmt\Foreach_;
 use Rector\Core\Rector\AbstractRector;
 use Rector\Naming\ExpectedNameResolver\InflectorSingularResolver;
 use Rector\Naming\Naming\VariableNaming;
 use Rector\NodeTypeResolver\Node\AttributeKey;
-use Rector\PostRector\Collector\NodesToAddCollector;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
 
@@ -30,7 +30,6 @@ final class DowngradeKeysInListRector extends AbstractRector
     public function __construct(
         private readonly InflectorSingularResolver $inflectorSingularResolver,
         private readonly VariableNaming $variableNaming,
-        private readonly NodesToAddCollector $nodesToAddCollector,
     ) {
     }
 
@@ -39,7 +38,7 @@ final class DowngradeKeysInListRector extends AbstractRector
      */
     public function getNodeTypes(): array
     {
-        return [List_::class, Array_::class];
+        return [Expression::class];
     }
 
     public function getRuleDefinition(): RuleDefinition
@@ -57,6 +56,7 @@ class SomeClass
             ["id" => 1, "name" => 'Tom'],
             ["id" => 2, "name" => 'Fred'],
         ];
+
         list("id" => $id1, "name" => $name1) = $data[0];
     }
 }
@@ -71,6 +71,7 @@ class SomeClass
             ["id" => 1, "name" => 'Tom'],
             ["id" => 2, "name" => 'Fred'],
         ];
+
         $id1 = $data[0]["id"];
         $name1 = $data[0]["name"];
     }
@@ -82,97 +83,128 @@ CODE_SAMPLE
     }
 
     /**
-     * @param List_|Array_ $node
+     * @param Expression $node
      */
-    public function refactor(Node $node): ?Node
+    public function refactor(Node $node)
     {
-        $parentNode = $node->getAttribute(AttributeKey::PARENT_NODE);
-        if (! $parentNode instanceof Node) {
-            return null;
-        }
-
-        $parentExpression = $parentNode->getAttribute(AttributeKey::PARENT_NODE);
-        if (! $parentExpression instanceof Node) {
-            return null;
-        }
-
-        $assignExpressions = $this->processExtractToItsOwnVariable($node, $parentNode, $parentExpression);
-        if ($assignExpressions === []) {
-            return null;
-        }
-
-        if ($parentNode instanceof Assign) {
-            $this->mirrorComments($assignExpressions[0], $parentExpression);
-            $this->nodesToAddCollector->addNodesBeforeNode($assignExpressions, $node);
-            $this->removeNode($parentExpression);
-
-            return $node;
-        }
-
-        if ($parentNode instanceof Foreach_) {
-            $defaultValueVar = $this->inflectorSingularResolver->resolve((string) $this->getName($parentNode->expr));
-            $scope = $parentNode->getAttribute(AttributeKey::SCOPE);
-            $newValueVar = $this->variableNaming->createCountedValueName($defaultValueVar, $scope);
-            $parentNode->valueVar = new Variable($newValueVar);
-            $stmts = $parentNode->stmts;
-
-            if ($stmts === []) {
-                $parentNode->stmts = $assignExpressions;
-            } else {
-                $this->nodesToAddCollector->addNodesBeforeNode($assignExpressions, $parentNode->stmts[0]);
+        if ($node instanceof Expression) {
+            if ($node->expr instanceof Assign) {
+                return $this->refactorAssignExpression($node);
             }
 
-            return $parentNode->valueVar;
+            return null;
+        }
+
+        if ($node instanceof Foreach_) {
+            if (! $node->expr instanceof Assign) {
+                return null;
+            }
+
+            /** @var Assign $assign */
+            $assign = $node->expr;
+            if (! $assign->var instanceof List_ && ! $assign->var instanceof Array_) {
+                return null;
+            }
+
+            $assignedArrayOrList = $assign->var;
+
+            $defaultValueVar = $this->inflectorSingularResolver->resolve((string) $this->getName($assign->var));
+            //            $scope = $parentNode->getAttribute(AttributeKey::SCOPE);
+
+            $scope = $node->getAttributes(AttributeKey::SCOPE);
+            $newValueVar = $this->variableNaming->createCountedValueName($defaultValueVar, $scope);
+            $node->valueVar = new Variable($newValueVar);
+            $stmts = $node->stmts;
+
+            $assignExpressions = $this->processExtractToItsOwnVariable($assignedArrayOrList, $assign, $expression);
+
+            if ($stmts === []) {
+                $node->stmts = $assignExpressions;
+            } else {
+                return [...$assignExpressions, $node->stmts[0]];
+            }
+
+            return $node->valueVar;
         }
 
         return null;
     }
 
     /**
-     * @return Expression[]
+     * @return Stmt[]
      */
-    private function processExtractToItsOwnVariable(List_ | Array_ $node, Node $parent, Node $parentExpression): array
-    {
+    private function processExtractToItsOwnVariable(
+        List_ | Array_ $node,
+        Assign $assign,
+        Expression|Foreach_ $expressionOrForeach
+    ): array {
         $items = $node->items;
-        $assignExpressions = [];
+
+        $assignStmts = [];
 
         foreach ($items as $item) {
             if (! $item instanceof ArrayItem) {
                 return [];
             }
 
-            /** keyed and not keyed cannot be mixed, return early */
+            // keyed and not keyed cannot be mixed, return early
             if (! $item->key instanceof Expr) {
                 return [];
             }
 
-            if ($parentExpression instanceof Expression && $parent instanceof Assign && $parent->var === $node) {
-                $assignExpressions[] = new Expression(
-                    new Assign($item->value, new ArrayDimFetch($parent->expr, $item->key))
+            if ($expressionOrForeach instanceof Expression && $assign instanceof Assign && $assign->var === $node) {
+                $assignStmts[] = new Expression(
+                    new Assign($item->value, new ArrayDimFetch($assign->expr, $item->key))
                 );
             }
 
-            if (! $parent instanceof Foreach_) {
+            if (! $assign instanceof Foreach_) {
                 continue;
             }
 
-            if ($parent->valueVar !== $node) {
+            if ($assign->valueVar !== $node) {
                 continue;
             }
 
-            $assignExpressions[] = $this->getExpressionFromForeachValue($parent, $item);
+            $assignStmts[] = $this->getExpressionFromForeachValue($assign, $item);
         }
 
-        return $assignExpressions;
+        return $assignStmts;
     }
 
     private function getExpressionFromForeachValue(Foreach_ $foreach, ArrayItem $arrayItem): Expression
     {
         $defaultValueVar = $this->inflectorSingularResolver->resolve((string) $this->getName($foreach->expr));
+
         $scope = $foreach->getAttribute(AttributeKey::SCOPE);
+
         $newValueVar = $this->variableNaming->createCountedValueName($defaultValueVar, $scope);
         $assign = new Assign($arrayItem->value, new ArrayDimFetch(new Variable($newValueVar), $arrayItem->key));
 
         return new Expression($assign);
+    }
+
+    /**
+     * @param Expression<Assign> $expression
+     * @return Stmt[]|null
+     */
+    private function refactorAssignExpression(Expression $expression): ?array
+    {
+        /** @var Assign $assign */
+        $assign = $expression->expr;
+        if (! $assign->var instanceof List_ && ! $assign->var instanceof Array_) {
+            return null;
+        }
+
+        $assignedArrayOrList = $assign->var;
+
+        $assignExpressions = $this->processExtractToItsOwnVariable($assignedArrayOrList, $assign, $expression);
+        if ($assignExpressions === []) {
+            return null;
+        }
+
+        $this->mirrorComments($assignExpressions[0], $expression);
+
+        return [$assignExpressions, $expression];
     }
 }
