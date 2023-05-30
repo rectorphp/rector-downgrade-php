2043caa02e2e817104c7dd35af1bd3a82d0d6f5d
Tomas Votruba <tomas.vot@gmail.com>
2023-05-30 10:36:41 +0200

    Remove node added from DowngradeArrayKeyFirstLastRector (#67)

---
+++ src/NodeAnalyzer/StmtMatcher.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\NodeAnalyzer;
+
+use PhpParser\Node\Expr\FuncCall;
+use PhpParser\Node\Name;
+use PhpParser\Node\Stmt;
+use Rector\Core\PhpParser\Node\BetterNodeFinder;
+
+final class StmtMatcher
+{
+    public function __construct(
+        private readonly BetterNodeFinder $betterNodeFinder,
+    ) {
+    }
+
+    public function matchFuncCallNamed(Stmt $stmt, string $functionName): ?FuncCall
+    {
+        /** @var FuncCall[] $funcCalls */
+        $funcCalls = $this->betterNodeFinder->findInstancesOf($stmt, [FuncCall::class]);
+
+        foreach ($funcCalls as $funcCall) {
+            if (! $funcCall->name instanceof Name) {
+                continue;
+            }
+
+            if ($funcCall->name->toString() !== $functionName) {
+                continue;
+            }
+
+            return $funcCall;
+        }
+
+        return null;
+    }
+}
