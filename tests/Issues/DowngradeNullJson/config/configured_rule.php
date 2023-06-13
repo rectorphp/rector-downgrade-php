ccace97b1136b8c48ebeea0d2a733349d2ab20b1
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-06-14 00:19:49 +0700

    Move more tests/Issues/IssueDowngrade* here

---
+++ tests/Issues/DowngradeNullJson/config/configured_rule.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\DowngradePhp73\Rector\ConstFetch\DowngradePhp73JsonConstRector;
+use Rector\DowngradePhp74\Rector\Coalesce\DowngradeNullCoalescingOperatorRector;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->import(__DIR__ . '/../../../../config/config.php');
+    $rectorConfig->rules([DowngradeNullCoalescingOperatorRector::class, DowngradePhp73JsonConstRector::class]);
+};
