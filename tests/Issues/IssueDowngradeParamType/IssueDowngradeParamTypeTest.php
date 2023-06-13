10d63bac978ee33e9d21d260a1b0a0a6f10eef63
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-06-14 00:19:44 +0700

    Move more tests/Issues/IssueDowngrade* here

--- tests/Issues/IssueDowngradeParamType/IssueDowngradeParamTypeTest.php
+++ tests/Issues/IssueDowngradeParamType/IssueDowngradeParamTypeTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Rector\Tests\Issues\IssueDowngradeParamType;
+namespace Rector\Tests\IssueseDowngradeParamType;
 
 use Iterator;
 use PHPUnit\Framework\Attributes\DataProvider;
