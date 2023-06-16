c1d7a3c16534ac97d77e9124652424fed3ce688f
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-06-16 10:38:32 +0700

    [DowngradePhp743] Handle return assign on ArrowFunctionToAnonymousFunctionRector

---
+++ rules-tests/DowngradePhp74/Rector/ArrowFunction/ArrowFunctionToAnonymousFunctionRector/Source/Foo.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Tests\DowngradePhp74\Rector\ArrowFunction\ArrowFunctionToAnonymousFunctionRector\Source;
+
+class Foo {
+    public $bar = 1;
+}
