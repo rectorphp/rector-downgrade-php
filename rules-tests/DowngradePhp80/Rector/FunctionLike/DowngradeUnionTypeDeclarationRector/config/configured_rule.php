738ee5d4aab488f3698449ff16339fc5f98bb418
Tomas Votruba <tomas.vot@gmail.com>
2023-08-06 01:55:49 +0200

    set exact

--- rules-tests/DowngradePhp80/Rector/FunctionLike/DowngradeUnionTypeDeclarationRector/config/configured_rule.php
+++ rules-tests/DowngradePhp80/Rector/FunctionLike/DowngradeUnionTypeDeclarationRector/config/configured_rule.php
@@ -4,10 +4,13 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 
+use Rector\Core\ValueObject\PhpVersion;
 use Rector\DowngradePhp80\Rector\FunctionLike\DowngradeUnionTypeDeclarationRector;
 use Rector\Tests\ConfigList;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->import(ConfigList::MAIN);
     $rectorConfig->rule(DowngradeUnionTypeDeclarationRector::class);
+
+    $rectorConfig->phpVersion(PhpVersion::PHP_80);
 };
