7057b455622413dc377df8420bf56ae10f5c7c74
Tomas Votruba <tomas.vot@gmail.com>
2023-09-01 12:28:34 +0200

    remove text fixture for skipped type widening, as used only for rector downgrade of symfony container, not needed since Rector 0.18

--- rules-tests/DowngradePhp72/Rector/ClassMethod/DowngradeParameterTypeWideningRector/config/configured_rule.php
+++ rules-tests/DowngradePhp72/Rector/ClassMethod/DowngradeParameterTypeWideningRector/config/configured_rule.php
@@ -2,16 +2,10 @@
 
 declare(strict_types=1);
 
-use Psr\Container\ContainerInterface;
-
 use Rector\Config\RectorConfig;
+
 use Rector\DowngradePhp72\Rector\ClassMethod\DowngradeParameterTypeWideningRector;
-use Rector\Tests\DowngradePhp72\Rector\ClassMethod\DowngradeParameterTypeWideningRector\Fixture\SomeContainerInterface;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig
-        ->ruleWithConfiguration(DowngradeParameterTypeWideningRector::class, [
-            ContainerInterface::class => ['set', 'get', 'has', 'initialized'],
-            SomeContainerInterface::class => ['set', 'has'],
-        ]);
+    $rectorConfig->rule(DowngradeParameterTypeWideningRector::class);
 };
