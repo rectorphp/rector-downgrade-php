c05f11e6120fb8ce0cb03b94d052912e8b72c6ae
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-12-11 10:51:25 +0700

    [Downgradephp82] Add DowngradeStandaloneNullTrueFalseReturnTypeRector

---
+++ rules-tests/DowngradePhp82/Rector/FunctionLike/DowngradeStandaloneNullTrueFalseReturnTypeRector/config/configured_rule.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+use Rector\DowngradePhp82\Rector\FunctionLike\DowngradeStandaloneNullTrueFalseReturnTypeRector;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->rule(DowngradeStandaloneNullTrueFalseReturnTypeRector::class);
+};
---
+++ rules/DowngradePhp82/Rector/FunctionLike/DowngradeStandaloneNullTrueFalseReturnTypeRector.php
@@ -0,0 +1,90 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\DowngradePhp82\Rector\FunctionLike;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr\Closure;
+use PhpParser\Node\FunctionLike;
+use PhpParser\Node\Identifier;
+use PhpParser\Node\Stmt\Class_;
+use PhpParser\Node\Stmt\ClassMethod;
+use PhpParser\Node\Stmt\Function_;
+use Rector\Core\Rector\AbstractRector;
+use Rector\Core\ValueObject\MethodName;
+use Rector\Privatization\NodeManipulator\VisibilityManipulator;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+/**
+ * @changelog https://wiki.php.net/rfc/null-false-standalone-types
+ * @changelog https://wiki.php.net/rfc/true-type
+ * @see \Rector\Tests\DowngradePhp82\Rector\FunctionLike\DowngradeStandaloneNullTrueFalseReturnTypeRector\DowngradeStandaloneNullTrueFalseReturnTypeRectorTest
+ */
+final class DowngradeStandaloneNullTrueFalseReturnTypeRector extends AbstractRector
+{
+    /**
+     * @return array<class-string<Node>>
+     */
+    public function getNodeTypes(): array
+    {
+        return [FunctionLike::class];
+    }
+
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition(
+            'Downgrade standalone return null, true, or false',
+            [
+                new CodeSample(
+                    <<<'CODE_SAMPLE'
+final class SomeClass
+{
+    public function run(): null
+    {
+        return null;
+    }
+}
+CODE_SAMPLE
+                    ,
+                    <<<'CODE_SAMPLE'
+final class SomeClass
+{
+    public function run(): mixed
+    {
+        return null;
+    }
+}
+CODE_SAMPLE
+                ),
+            ]
+        );
+    }
+
+    /**
+     * @param FunctionLike $node
+     */
+    public function refactor(Node $node): ?Node
+    {
+        $returnType = $node->getReturnType();
+        if (! $returnType instanceof Node) {
+            return null;
+        }
+
+        $returnTypeName = $this->getName($returnType);
+        if (! in_array($returnTypeName, ['null', 'false', 'true'])) {
+            return null;
+        }
+
+
+        if (! $node instanceof ClassMethod) {
+            $node->returnType = new Identifier('mixed');
+            return $node;
+        }
+
+        // todo: verify parent
+        $node->returnType = new Identifier('mixed');
+        return null;
+    }
+}
