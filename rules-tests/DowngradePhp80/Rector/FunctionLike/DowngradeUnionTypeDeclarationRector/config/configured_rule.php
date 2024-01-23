e9b037d7be1101251e665550ddc7c9601265ebbc
GitHub Action <actions@github.com>
2024-01-23 10:43:18 +0000

    [ci-review] Rector Rectify

--- rules-tests/DowngradePhp80/Rector/FunctionLike/DowngradeUnionTypeDeclarationRector/config/configured_rule.php
+++ rules-tests/DowngradePhp80/Rector/FunctionLike/DowngradeUnionTypeDeclarationRector/config/configured_rule.php
@@ -3,7 +3,6 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-
 use Rector\DowngradePhp80\Rector\FunctionLike\DowngradeUnionTypeDeclarationRector;
 use Rector\ValueObject\PhpVersion;
 
