a16ded4032840457048afbd40bf31e1f66df1154
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-06-13 08:26:07 +0700

    [DowngradePhp80] Handle return throw ternary on DowngradeThrowExprRector (#224)
    * [DowngradePhp80] Handle return throw ternary on DowngradeThrowExprRector
    
    * [ci-review] Rector Rectify
    
    * Fix phpstan
    
    * Fix phpstan
    
    * Fix phpstan
    
    ---------
    
    Co-authored-by: GitHub Action <actions@github.com>

--- rules/DowngradePhp80/Rector/Expression/DowngradeThrowExprRector.php
+++ rules/DowngradePhp80/Rector/Expression/DowngradeThrowExprRector.php
@@ -211,6 +211,15 @@ CODE_SAMPLE
             return [new Expression($return->expr)];
         }
 
+        if ($return->expr instanceof Ternary) {
+            $if = $this->refactorTernary($return->expr, null);
+            if (! $if instanceof If_) {
+                return null;
+            }
+
+            return [$if, new Return_($return->expr->cond)];
+        }
+
         return null;
     }
 
