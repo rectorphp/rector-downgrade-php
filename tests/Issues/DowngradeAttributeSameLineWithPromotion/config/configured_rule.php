4201862a5e8b969f461e95767c1d280a912709ed
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-11-25 03:12:58 +0700

    [DowngradePhp80] Downgrade same line attribute on non-removed attribute on DowngradeAttributeToAnnotationRector (#251)
    * [DowngradePhp80] Downgrade same line attribute on non-removed attribute on DowngradeAttributeToAnnotationRector
    
    * [ci-review] Rector Rectify
    
    ---------
    
    Co-authored-by: GitHub Action <actions@github.com>

---
+++ tests/Issues/DowngradeAttributeSameLineWithPromotion/config/configured_rule.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\DowngradePhp80\Rector\Class_\DowngradeAttributeToAnnotationRector;
+use Rector\DowngradePhp80\Rector\Class_\DowngradePropertyPromotionRector;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->rules([DowngradeAttributeToAnnotationRector::class, DowngradePropertyPromotionRector::class]);
+};
