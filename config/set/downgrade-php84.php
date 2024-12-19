978bfeecc187b94a9025394858342a48fe07c148
Jorg Sowa <jorg.sowa@gmail.com>
2024-12-20 00:18:22 +0100

    Registered rule to downgrade-php84 ruleset

--- config/set/downgrade-php84.php
+++ config/set/downgrade-php84.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\DowngradePhp84\Rector\FuncCall\DowngradeRoundingModeEnumRector;
 use Rector\DowngradePhp84\Rector\MethodCall\DowngradeNewMethodCallWithoutParenthesesRector;
 use Rector\ValueObject\PhpVersion;
 
@@ -10,5 +11,6 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->phpVersion(PhpVersion::PHP_83);
     $rectorConfig->rules([
         DowngradeNewMethodCallWithoutParenthesesRector::class,
+        DowngradeRoundingModeEnumRector::class,
     ]);
 };
