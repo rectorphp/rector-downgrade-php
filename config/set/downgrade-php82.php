9d0a7b24349833f3e95829006d5dfec83525f605
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-01-01 21:09:19 +0700

    [DowngradePhp82] Add DowngradeIteratorCountToArrayRector (#261)
    * [DowngradePhp82] Add DowngradeIteratorCountToArrayRector
    
    * cs fix
    
    * Fix phpstan
    
    * Fix phpstan
    
    * fix

--- config/set/downgrade-php82.php
+++ config/set/downgrade-php82.php
@@ -5,12 +5,14 @@ declare(strict_types=1);
 use Rector\Config\RectorConfig;
 use Rector\ValueObject\PhpVersion;
 use Rector\DowngradePhp82\Rector\Class_\DowngradeReadonlyClassRector;
+use Rector\DowngradePhp82\Rector\FuncCall\DowngradeIteratorCountToArrayRector;
 use Rector\DowngradePhp82\Rector\FunctionLike\DowngradeStandaloneNullTrueFalseReturnTypeRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->phpVersion(PhpVersion::PHP_81);
     $rectorConfig->rules([
         DowngradeReadonlyClassRector::class,
-        DowngradeStandaloneNullTrueFalseReturnTypeRector::class
+        DowngradeStandaloneNullTrueFalseReturnTypeRector::class,
+        DowngradeIteratorCountToArrayRector::class,
     ]);
 };
