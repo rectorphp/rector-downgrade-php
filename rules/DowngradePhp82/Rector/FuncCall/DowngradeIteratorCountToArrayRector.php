f9cc5a03dc2a894691d3878adb6e9eef3483281a
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-01-01 21:37:24 +0700

    [Downgradephp82] Skip in ternary on DowngradeIteratorCountToArrayRector (#262)
    * [Downgradephp82] Skip in ternary on DowngradeIteratorCountToArrayRector
    
    * Fix
    
    * skip nullable object
    
    * [ci-review] Rector Rectify
    
    ---------
    
    Co-authored-by: GitHub Action <actions@github.com>

--- rules/DowngradePhp82/Rector/FuncCall/DowngradeIteratorCountToArrayRector.php
+++ rules/DowngradePhp82/Rector/FuncCall/DowngradeIteratorCountToArrayRector.php
@@ -10,8 +10,11 @@ use PhpParser\Node\Expr\FuncCall;
 use PhpParser\Node\Expr\New_;
 use PhpParser\Node\Expr\Ternary;
 use PhpParser\Node\Name\FullyQualified;
+use PhpParser\NodeTraverser;
 use PHPStan\Type\Type;
+use PHPStan\Type\TypeCombinator;
 use Rector\NodeAnalyzer\ArgsAnalyzer;
+use Rector\PhpParser\Node\BetterNodeFinder;
 use Rector\Rector\AbstractRector;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
@@ -26,7 +29,8 @@ use Webmozart\Assert\Assert;
 final class DowngradeIteratorCountToArrayRector extends AbstractRector
 {
     public function __construct(
-        private readonly ArgsAnalyzer $argsAnalyzer
+        private readonly ArgsAnalyzer $argsAnalyzer,
+        private readonly BetterNodeFinder $betterNodeFinder
     ) {
     }
 
@@ -35,7 +39,7 @@ final class DowngradeIteratorCountToArrayRector extends AbstractRector
      */
     public function getNodeTypes(): array
     {
-        return [FuncCall::class];
+        return [Ternary::class, FuncCall::class];
     }
 
     public function getRuleDefinition(): RuleDefinition
@@ -63,10 +67,23 @@ CODE_SAMPLE
     }
 
     /**
-     * @param FuncCall $node
+     * @param Ternary|FuncCall $node
      */
-    public function refactor(Node $node): ?Node
+    public function refactor(Node $node): null|FuncCall|int
     {
+        if ($node instanceof Ternary) {
+            $hasIsArrayCheck = (bool) $this->betterNodeFinder->findFirst(
+                $node,
+                fn (Node $subNode): bool => $subNode instanceof FuncCall && $this->isName($subNode, 'is_array')
+            );
+
+            if ($hasIsArrayCheck) {
+                return NodeTraverser::DONT_TRAVERSE_CHILDREN;
+            }
+
+            return null;
+        }
+
         if (! $this->isNames($node, ['iterator_count', 'iterator_to_array'])) {
             return null;
         }
@@ -107,6 +124,7 @@ CODE_SAMPLE
             return false;
         }
 
+        $type = TypeCombinator::removeNull($type);
         return $type->isObject()
             ->yes();
     }
