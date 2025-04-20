da5e218de8a4ffb056fd842dee1843f945256a83
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-04-20 09:29:25 +0700

    [DowngradePhp80] Handle union implements method from interface on DowngradeUnionTypeDeclarationRector

---
+++ rules-tests/DowngradePhp80/Rector/FunctionLike/DowngradeUnionTypeDeclarationRector/Source/Foo1.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Rector\Tests\DowngradePhp80\Rector\FunctionLike\DowngradeUnionTypeDeclarationRector\Source;
+
+class Foo1 implements FooInterface
+{
+}
---
+++ rules-tests/DowngradePhp80/Rector/FunctionLike/DowngradeUnionTypeDeclarationRector/Source/Foo2.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Rector\Tests\DowngradePhp80\Rector\FunctionLike\DowngradeUnionTypeDeclarationRector\Source;
+
+class Foo2 implements FooInterface
+{
+}
