9d2e956a47127bcc2a1c901d6a5f1ff7fc25c753
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-05-10 21:49:17 +0700

    Add is_object() on DowngradeInstanceofStringableRector when possibly not object type (#276)
    * Add is_object() on DowngradeInstanceofStringableRector
    
    * isobject

--- rules/DowngradePhp80/Rector/Instanceof_/DowngradeInstanceofStringableRector.php
+++ rules/DowngradePhp80/Rector/Instanceof_/DowngradeInstanceofStringableRector.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Rector\DowngradePhp80\Rector\Instanceof_;
 
 use PhpParser\Node;
+use PhpParser\Node\Expr\BinaryOp\BooleanAnd;
+use PhpParser\Node\Expr\FuncCall;
 use PhpParser\Node\Expr\Instanceof_;
 use PhpParser\Node\Name\FullyQualified;
 use PhpParser\Node\Scalar\String_;
@@ -28,7 +30,7 @@ CODE_SAMPLE
 
                 ,
                 <<<'CODE_SAMPLE'
-method_exists($obj, '__toString');
+is_object($obj) && method_exists($obj, '__toString');
 CODE_SAMPLE
             ),
         ]);
@@ -45,7 +47,7 @@ CODE_SAMPLE
     /**
      * @param Instanceof_ $node
      */
-    public function refactor(Node $node): ?Node
+    public function refactor(Node $node): null|FuncCall|BooleanAnd
     {
         if (! $node->class instanceof FullyQualified) {
             return null;
@@ -55,6 +57,13 @@ CODE_SAMPLE
             return null;
         }
 
-        return $this->nodeFactory->createFuncCall('method_exists', [$node->expr, new String_('__toString')]);
+        $nativeExprType = $this->nodeTypeResolver->getNativeType($node->expr);
+        $funcCall = $this->nodeFactory->createFuncCall('method_exists', [$node->expr, new String_('__toString')]);
+
+        if ($nativeExprType->isObject()->yes()) {
+            return $funcCall;
+        }
+
+        return new BooleanAnd($this->nodeFactory->createFuncCall('is_object', [$node->expr]), $funcCall);
     }
 }
