76925a548a5469471d1ede5b749b54f31e7a4c34
GitHub Action <actions@github.com>
2025-05-23 08:05:38 +0000

    [ci-review] Rector Rectify

--- tests/Issues/DowngradeNamedTrailing/config/configured_rule.php
+++ tests/Issues/DowngradeNamedTrailing/config/configured_rule.php
@@ -1,11 +1,10 @@
 <?php
 
+declare(strict_types=1);
+
 use Rector\Config\RectorConfig;
-use Rector\DowngradePhp80\Rector\MethodCall\DowngradeNamedArgumentRector;
 use Rector\DowngradePhp73\Rector\FuncCall\DowngradeTrailingCommasInFunctionCallsRector;
+use Rector\DowngradePhp80\Rector\MethodCall\DowngradeNamedArgumentRector;
 
 return RectorConfig::configure()
-    ->withRules([
-        DowngradeNamedArgumentRector::class,
-        DowngradeTrailingCommasInFunctionCallsRector::class,
-    ]);
+    ->withRules([DowngradeNamedArgumentRector::class, DowngradeTrailingCommasInFunctionCallsRector::class]);
