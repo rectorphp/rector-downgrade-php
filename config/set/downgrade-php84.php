197f088e31056bda3e3e0f1a368b368990b5aeed
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-07-11 15:14:44 +0700

    [DowngradePhp84] Add DowngradeArrayFindRector (#292)
    * [DowngradePhp84] Add DowngradeArrayFindRector
    
    * [DowngradePhp84] Add DowngradeArrayFindRector
    
    * [DowngradePhp84] Add DowngradeArrayFindRector
    
    * [DowngradePhp84] Add DowngradeArrayFindRector

--- config/set/downgrade-php84.php
+++ config/set/downgrade-php84.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use Rector\Config\RectorConfig;
 use Rector\DowngradePhp84\Rector\Expression\DowngradeArrayAllRector;
 use Rector\DowngradePhp84\Rector\Expression\DowngradeArrayAnyRector;
+use Rector\DowngradePhp84\Rector\Expression\DowngradeArrayFindRector;
 use Rector\DowngradePhp84\Rector\FuncCall\DowngradeRoundingModeEnumRector;
 use Rector\DowngradePhp84\Rector\MethodCall\DowngradeNewMethodCallWithoutParenthesesRector;
 use Rector\ValueObject\PhpVersion;
@@ -16,5 +17,6 @@ return static function (RectorConfig $rectorConfig): void {
         DowngradeRoundingModeEnumRector::class,
         DowngradeArrayAllRector::class,
         DowngradeArrayAnyRector::class,
+        DowngradeArrayFindRector::class,
     ]);
 };
