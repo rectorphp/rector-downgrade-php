c8ae2279abfe09d782f234dcdae35d73cb898a7c
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-09-08 14:06:11 +0700

    [DowngradePhp80] Register Override and SensitiveParameter to skipped attributes on DowngradeAttributeToAnnotationRector

--- rules/DowngradePhp80/Rector/Class_/DowngradeAttributeToAnnotationRector.php
+++ rules/DowngradePhp80/Rector/Class_/DowngradeAttributeToAnnotationRector.php
@@ -34,7 +34,13 @@ final class DowngradeAttributeToAnnotationRector extends AbstractRector implemen
     /**
      * @var string[]
      */
-    private const SKIPPED_ATTRIBUTES = ['Attribute', 'ReturnTypeWillChange', 'AllowDynamicProperties'];
+    private const SKIPPED_ATTRIBUTES = [
+        'Attribute',
+        'ReturnTypeWillChange',
+        'AllowDynamicProperties',
+        'Override',
+        'SensitiveParameter',
+    ];
 
     /**
      * @var DowngradeAttributeToAnnotation[]
