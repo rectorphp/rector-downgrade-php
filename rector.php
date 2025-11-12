0effdc8a79da2f64233e2f37e20edf0a7c146c96
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-11-13 00:00:38 +0700

    ignore in rector

--- rector.php
+++ rector.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 use Rector\CodingStyle\Rector\String_\UseClassKeywordForClassNameResolutionRector;
 use Rector\Config\RectorConfig;
+use Rector\DeadCode\Rector\If_\RemoveAlwaysTrueIfConditionRector;
+use Rector\DeadCode\Rector\If_\RemoveDeadInstanceOfRector;
 use Rector\Php55\Rector\String_\StringClassNameToClassConstantRector;
 use Rector\PHPUnit\Set\PHPUnitSetList;
 
@@ -35,6 +37,16 @@ return RectorConfig::configure()
             // not useful short class cases, possibly skip in the rule
             __DIR__ . '/rules/DowngradePhp80/Rector/MethodCall/DowngradeReflectionClassGetConstantsFilterRector.php',
             __DIR__ . '/rules/DowngradePhp81/Rector/FuncCall/DowngradeFirstClassCallableSyntaxRector.php',
+        ],
+
+        RemoveAlwaysTrueIfConditionRector::class => [
+            // false positive in or check usage
+            __DIR__ . '/rules/DowngradePhp80/Rector/NullsafeMethodCall/DowngradeNullsafeToTernaryOperatorRector.php',
+        ],
+
+        RemoveDeadInstanceOfRector::class => [
+            // false positive in or check usage
+            __DIR__ . '/rules/DowngradePhp80/Rector/NullsafeMethodCall/DowngradeNullsafeToTernaryOperatorRector.php',
         ],
 
         // test paths
