faccd122a07d313ccc248337dca7002c4d311417
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-12-20 23:19:42 +0700

    Add comment for reset counter on visit new file

--- rules/DowngradePhp80/Rector/NullsafeMethodCall/DowngradeNullsafeToTernaryOperatorRector.php
+++ rules/DowngradePhp80/Rector/NullsafeMethodCall/DowngradeNullsafeToTernaryOperatorRector.php
@@ -54,6 +54,9 @@ CODE_SAMPLE
         static $currentFile = null;
 
         if ($currentFile !== $this->file->getFilePath()) {
+            // the counter need start from 0 when visit new file to avoid random increment 
+            // accross files
+            // due to run on parallel
             $this->counter = 0;
             $currentFile = $this->file->getFilePath();
         }
