2e9b37bcf7f487b8c449aa5b6cab3633ba3b0e56
Abdul Malik Ikhsan <samsonasik@gmail.com>
2026-05-12 17:49:41 +0700

    fix phpstan

--- tests/Issues/IssueDowngradeMixedType/config/configured_rule.php
+++ tests/Issues/IssueDowngradeMixedType/config/configured_rule.php
@@ -2,18 +2,15 @@
 
 declare(strict_types=1);
 
+use Jetbrains\PhpStorm\Language;
+use Nette\DI\Attributes\Inject;
 use Rector\Config\RectorConfig;
 use Rector\DowngradePhp74\Rector\Property\DowngradeTypedPropertyRector;
-use Rector\DowngradePhp80\Rector\FunctionLike\DowngradeMixedTypeDeclarationRector;
 use Rector\DowngradePhp80\Rector\Class_\DowngradeAttributeToAnnotationRector;
+use Rector\DowngradePhp80\Rector\FunctionLike\DowngradeMixedTypeDeclarationRector;
 use Rector\DowngradePhp80\ValueObject\DowngradeAttributeToAnnotation;
 
 return static function (RectorConfig $rectorConfig): void {
-    /** @var class-string $netteInjectAttribute */
-    $netteInjectAttribute = 'Nette\DI\Attributes\Inject';
-    /** @var class-string $jetbrainsLanguageAttribute */
-    $jetbrainsLanguageAttribute = 'Jetbrains\PhpStorm\Language';
-
     $rectorConfig->importNames();
     $rectorConfig->rule(DowngradeTypedPropertyRector::class);
     $rectorConfig->rule(DowngradeMixedTypeDeclarationRector::class);
@@ -23,8 +20,8 @@ return static function (RectorConfig $rectorConfig): void {
             // Symfony
             new DowngradeAttributeToAnnotation('Symfony\Contracts\Service\Attribute\Required', 'required'),
             // Nette
-            new DowngradeAttributeToAnnotation($netteInjectAttribute, 'inject'),
+            new DowngradeAttributeToAnnotation(Inject::class, 'inject'),
             // Jetbrains\PhpStorm\Language under nette/utils
-            new DowngradeAttributeToAnnotation($jetbrainsLanguageAttribute, 'language'),
+            new DowngradeAttributeToAnnotation(Language::class, 'language'),
         ]);
 };
